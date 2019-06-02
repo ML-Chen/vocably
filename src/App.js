f662ef591878ac34778c740b45c5c6b5e0a18e33
Michael Chen <flamingdescent@gmail.com>
2019-06-02 14:35:12 -0400

    Add some functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Speech from 'speak-tts'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      play: true
+    }
+    this.vocabDict = {
+      "hi": "hello",
+      "now": "newt",
+      "yo": "what"
+    }
+    this.speech = new Speech()
+    if (this.speech.hasBrowserSupport())
+      console.log("Speech synthesis supported by browser")
+    this.speech.init().then(data => {
+      console.log("Speech is ready, voices are available", data)
+    }).catch(e => {
+      console.log("An error occurred while initializing: ", e)
+    })
+  }
+
+  randomKey = dict => {
+    const keys = Object.keys(dict) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+    return keys[keys.length * Math.random() << 0]
+  }
+  
+  render() {
+    while (this.state.play) {
+      (async () => {
+        const randomKey = randomKey(this.vocabDict)
+        await this.speech.speak({
+          text: randomKey
+        })
+        await this.speech.speak({
+          text: this.vocabDict[randomKey]
+        })
+      })();
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Not Glossika</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
