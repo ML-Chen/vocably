4024ec7dca86fd7d90b2a8f5e637057f40640f06
Michael Chen <flamingdescent@gmail.com>
2019-06-08 09:15:38 -0400

    Add speakPolly function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css'
 import Speech from 'speak-tts'
 // import hsk5 from './hsk5.js'
 import hsk56 from './hsk5+6.js'
+import AWS from 'aws-sdk'
 
 const speech = new Speech()
+const polly = new AWS.Polly()
+let availableVoices = []
 
 const sleep = ms => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Gets a random item from a list
 const randomItem = list => {
   return new Promise((resolve, reject) => {
     if (list === null || list === [])
@@ -22,10 +26,11 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      item: null,
+      item: null, // object from hsk5+6.js
       play: true,
-      progress: 0, // number representing what line to continue with speaking after paused
-      rows: [] // lists previously said words
+      progress: 0, // basically whether to say the hanzi, pinyin, or English next
+      rows: [], // list of previously said words
+      usePolly: true // whether to use Polly for hanzi. Browser TTS will still be used for English.
     }
     if (speech.hasBrowserSupport())
       console.log("Speech synthesis supported by browser")
@@ -34,6 +39,15 @@ class App extends Component {
     }).catch(err => {
       console.log("An error occurred while initializing: ", err)
     })
+
+    if (this.state.usePolly) {
+      polly.describeVoices({}, (err, data) => {
+        if (err)
+          console.log(err, err.stack)
+        else
+          availableVoices = data.Voices
+      })
+    }
   }
 
   resume = async () => {
@@ -49,13 +63,13 @@ class App extends Component {
   }
 
   /**
-   * Sets the rate and language (if given) and then speaks the text using TTS.
+   * Sets the rate and language (if given) and then speaks the text using the browser TTS.
    * 
    * @param {string} text
-   * @param {number} [rate]
+   * @param {number} [rate] - e.g., 0.5 speaks at 50% the speed.
    * @param {string} [language] - e.g., 'zh-CN', 'en-US'
    */
-  speak = async (text, rate, language) => {
+  speakBrowser = async (text, rate, language) => {
     if (language)
       await speech.setLanguage(language)
     if (rate)
@@ -64,6 +78,39 @@ class App extends Component {
       text,
       listeners: { onend: () => {} }
     })
+  }
+
+  /**
+   * Speaks the given text using Amazon Polly. Assumes that the language is either Chinese (contains 'zh') or otherwise English.
+   * 
+   * @param {string} text
+   * @param {number} [rate] - e.g., 0.5 speaks at 50% the speed.
+   * @param {string} [language] - e.g., 'zh-CN', 'en-US'
+   */
+  speakPolly = async (text, rate, language) => {
+    text = rate ?
+      `<speak><prosody rate="${rate * 100 << 0}%">${text}</prosody></speak>` :
+      `<speak>${text}</speak>`
+    await polly.synthesizeSpeech({
+      OutputFormat: 'mp3',
+      Text: SSML,
+      VoiceId: language.includes('zh') ? "Zhiyu" : "Ivy"
+    })
+  }
+
+  /**
+   * Speaks the text with the given rate and language, using Polly if the given language is Chinese, and using the Browser TTS if otherwise. For browser TTS, the rate and language is *set*, so it will be what is used by any subsequent browser TTS calls.
+   * 
+   * @param {string} text
+   * @param {number} [rate] - e.g., 0.5 speaks at 50% the speed.
+   * @param {string} [language] - e.g., 'zh-CN', 'en-US'
+   */
+  speak = async (text, rate, language) => {
+    if (language.includes('zh')) {
+      await this.speakPolly(text, rate, language)
+    } else {
+      await this.speakBrowser(text, rate, language)
+    }
   }
 
   speakLoop = async (progress = -1) => {
